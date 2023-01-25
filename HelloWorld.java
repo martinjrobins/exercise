60ba59f9fec5d436230df0c621f4aac57946c630
Ferriswheel09 <fjiwad@gwu.edu>
2023-01-25 11:50:11 -0500

    Made a few changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld{
 	public static void main(int[] args){
 	
 		System.out.println("Hello World");
-		System.out.println("Doing this in the new branch");
-		System.out.println("I think this is how I do it");
+		System.out.println("New change again");
 	}
 }
