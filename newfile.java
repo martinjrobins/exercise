ac9f6eb337810fdc45e81c9a82df7b1dbb0823ab
Nada <nada@Nadas-MacBook-Pro.local>
2023-01-25 12:11:38 -0500

    error hopefully

--- newfile.java
+++ newfile.java
@@ -2,5 +2,7 @@ public class newfile{
     public static void main(String[] args){
        System.out.println("Hello World!"); 
        System.out.println("adding to new branch");
+       System.out.println("trying to get a conflict");
+
     }
  }
