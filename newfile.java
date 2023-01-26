02117d2f554d6636bb9d8fccc762db835f93134d
Nada <nada@Nadas-MacBook-Pro.local>
2023-01-26 16:58:55 -0500

    trying to get error

--- newfile.java
+++ newfile.java
@@ -1,8 +1,7 @@
 public class newfile{
     public static void main(String[] args){
        System.out.println("Hello World!"); 
-       System.out.println("adding to new branch");
-       System.out.println("trying to get a conflict");
+       System.out.println("attempting for an error");
 
     }
  }
