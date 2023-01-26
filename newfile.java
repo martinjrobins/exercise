f4f74c86ba9765211143295695f4bbdf7a778a61
Nada <nada@Nadas-MacBook-Pro.local>
2023-01-26 17:00:31 -0500

    changed line

--- newfile.java
+++ newfile.java
@@ -1,6 +1,6 @@
 public class newfile{
     public static void main(String[] args){
        System.out.println("Hello World!"); 
-       System.out.println("adding to new branch");
+       System.out.println("now with different text");
     }
  }
